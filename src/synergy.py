108e472c4e2f1d58aaa8878395f89eed98655c07
Finn Bjerknes <fmb1@rice.edu>
2022-02-15 02:58:22 -0600

    4 factors done

--- src/synergy.py
+++ src/synergy.py
@@ -14,6 +14,16 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 # Import pandas allowing us to export Lists of Lists to excel files.
 import pandas as pd
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file'
+    ]
+file_name = 'client_key.json'
+creds = ServiceAccountCredentials.from_json_keyfile_name(file_name,scope)
+client = gspread.authorize(creds)
 
 
 def teamData(team):
@@ -55,21 +65,21 @@ def teamData(team):
     teamName = driver.find_element(By.NAME, "ctl00$MainContent$lstTeam")
     teamName.send_keys(team)
 
-    time.sleep(10)
+    time.sleep(5)
 
     offData = driver.find_element(By.XPATH, '//td[@id="mainBackgroundColumn"]').text
 
     defButton = driver.find_element(By.LINK_TEXT, "Defensive")
     defButton.click()
 
-    time.sleep(10)
+    time.sleep(5)
 
     defData = driver.find_element(By.XPATH, '//td[@id="mainBackgroundColumn"]').text
 
     cbButton = driver.find_element(By.LINK_TEXT, "Cumulative Box")
     cbButton.click()
 
-    time.sleep(10)
+    time.sleep(5)
 
     cbData = driver.find_element(By.XPATH, '//td[@id="mainBackgroundColumn"]').text
 
@@ -154,15 +164,172 @@ def process_box(data):
     return vals
 
 
-def google(tables, rice, sheet_name):
+def google(tables, rice, sheet_name, team_name):
     print(tables)
     print(rice)
     print(sheet_name)
 
+    sheet = client.open('RBAG Scouting Reports 2021-22').worksheet(sheet_name)
+
+    # 4 factors
+    sheet.update_cell(12, 7, tables[0]['Overall Offense'][team_name][10])
+    sheet.update_cell(12, 8, tables[0]['Overall Offense'][team_name][4][:-1])
+    sheet.update_cell(13, 7, tables[0]['Overall Offense'][team_name][11])
+    sheet.update_cell(14, 7, str(round((float(tables[2][12]) / float(tables[2][11])) * 100, 2)) + '%')
+    sheet.update_cell(15, 7, tables[0]['Overall Offense'][team_name][12])
+
+    sheet.update_cell(16, 7, tables[0]['Shot Attempt - Half Court']['Around Basket (not Post-Ups)'][0])
+    sheet.update_cell(17, 7, tables[0]['Shot Attempt - Half Court']['Around Basket (not Post-Ups)'][9])
+    sheet.update_cell(16, 8, tables[0]['Shot Attempt - Half Court']['Around Basket (not Post-Ups)'][4][:-1])
+
+    mid_rate = (float(tables[0]['Jump Shot Range - half court']["Short (<17')"][0][:-1]) +
+               float(tables[0]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][0][:-1])) / 100
+    sheet.update_cell(18, 7, str(round(float(tables[0]['Shot Attempt - Half Court']['Jump Shots'][0][:-1]) * mid_rate, 2)) + '%')
+    mid_makes = float(tables[0]['Jump Shot Range - half court']["Short (<17')"][7]) + \
+               float(tables[0]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][7])
+    mid_total = float(tables[0]['Jump Shot Range - half court']["Short (<17')"][8]) + \
+               float(tables[0]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][8])
+    sheet.update_cell(19, 7, str(round(100 * mid_makes / mid_total, 2)) + '%')
+    mid_pct = (int(tables[0]['Jump Shot Range - half court']["Short (<17')"][4][:-1]) +
+               int(tables[0]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][4][:-1])) // 2
+    sheet.update_cell(18, 8, mid_pct)
+
+    three_rate = float(tables[0]['Shot Attempt - Half Court']['Jump Shots'][0][:-1]) * \
+               float(tables[0]['Jump Shot Range - half court']["Long (3 point jump shots)"][0][:-1]) / 100
+    sheet.update_cell(20, 7, str(round(three_rate, 2)) + '%')
+    sheet.update_cell(21, 7, tables[0]['Jump Shot Range - half court']["Long (3 point jump shots)"][9])
+    sheet.update_cell(20, 8, tables[0]['Jump Shot Range - half court']["Long (3 point jump shots)"][4][:-1])
+
+    sheet.update_cell(22, 7, tables[2][30])
+    sheet.update_cell(23, 7, str(100 * float(tables[2][5]) / float(tables[2][15])) + '%')
+
+    steal = float(tables[2][8])
+    block = float(tables[2][10])
+    def_poss = float(tables[1]['Overall Defense'][team_name][1])
+    sheet.update_cell(24, 7, str(100 * steal / def_poss) + '%')
+    sheet.update_cell(25, 7, str(100 * block / def_poss) + '%')
+
+    sheet.update_cell(26, 7, tables[0]['Overall Offense']['Transition'][0])
+    sheet.update_cell(27, 7, tables[0]['Overall Offense']['Transition'][10])
+    sheet.update_cell(26, 8, tables[0]['Overall Offense']['Transition'][4][:-1])
+
+    # -------
+
+    sheet.update_cell(12, 9, tables[1]['Overall Defense'][team_name][10])
+    sheet.update_cell(12, 10, tables[1]['Overall Defense'][team_name][4][:-1])
+    sheet.update_cell(13, 9, tables[1]['Overall Defense'][team_name][11])
+    sheet.update_cell(15, 9, tables[1]['Overall Defense'][team_name][12])
+
+    sheet.update_cell(16, 9, tables[1]['Shot Attempt - Half Court']['Around Basket (not Post-Ups)'][0])
+    sheet.update_cell(17, 9, tables[1]['Shot Attempt - Half Court']['Around Basket (not Post-Ups)'][9])
+    sheet.update_cell(16, 10, tables[1]['Shot Attempt - Half Court']['Around Basket (not Post-Ups)'][4][:-1])
+
+    mid_rate = (float(tables[1]['Jump Shot Range - half court']["Short (<17')"][0][:-1]) +
+                float(tables[1]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][0][:-1])) / 100
+    sheet.update_cell(18, 9, str(
+        round(float(tables[1]['Shot Attempt - Half Court']['Jump Shots'][0][:-1]) * mid_rate, 2)) + '%')
+    mid_makes = float(tables[1]['Jump Shot Range - half court']["Short (<17')"][7]) + \
+                float(tables[1]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][7])
+    mid_total = float(tables[1]['Jump Shot Range - half court']["Short (<17')"][8]) + \
+                float(tables[1]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][8])
+    sheet.update_cell(19, 9, str(round(100 * mid_makes / mid_total, 2)) + '%')
+    mid_pct = (int(tables[1]['Jump Shot Range - half court']["Short (<17')"][4][:-1]) +
+               int(tables[1]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][4][:-1])) // 2
+    sheet.update_cell(18, 10, mid_pct)
+
+    three_rate = float(tables[1]['Shot Attempt - Half Court']['Jump Shots'][0][:-1]) * \
+                 float(tables[1]['Jump Shot Range - half court']["Long (3 point jump shots)"][0][:-1]) / 100
+    sheet.update_cell(20, 9, str(round(three_rate, 2)) + '%')
+    sheet.update_cell(21, 9, tables[1]['Jump Shot Range - half court']["Long (3 point jump shots)"][9])
+    sheet.update_cell(20, 10, tables[1]['Jump Shot Range - half court']["Long (3 point jump shots)"][4][:-1])
+
+    sheet.update_cell(26, 9, tables[1]['Overall Defense']['Transition'][0])
+    sheet.update_cell(27, 9, tables[1]['Overall Defense']['Transition'][10])
+    sheet.update_cell(26, 10, tables[1]['Overall Defense']['Transition'][4][:-1])
+
+    # ----------------------------------------------------------------------------------------------------------
+
+    sheet.update_cell(12, 11, rice[0]['Overall Offense']["Rice Owls"][10])
+    sheet.update_cell(12, 12, rice[0]['Overall Offense']["Rice Owls"][4][:-1])
+    sheet.update_cell(13, 11, rice[0]['Overall Offense']["Rice Owls"][11])
+    sheet.update_cell(14, 11, str(round((float(rice[2][12]) / float(rice[2][11])) * 100, 2)) + '%')
+    sheet.update_cell(15, 11, rice[0]['Overall Offense']["Rice Owls"][12])
+
+    sheet.update_cell(16, 11, rice[0]['Shot Attempt - Half Court']['Around Basket (not Post-Ups)'][0])
+    sheet.update_cell(17, 11, rice[0]['Shot Attempt - Half Court']['Around Basket (not Post-Ups)'][9])
+    sheet.update_cell(16, 12, rice[0]['Shot Attempt - Half Court']['Around Basket (not Post-Ups)'][4][:-1])
+
+    mid_rate = (float(rice[0]['Jump Shot Range - half court']["Short (<17')"][0][:-1]) +
+                float(rice[0]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][0][:-1])) / 100
+    sheet.update_cell(18, 11, str(
+        round(float(rice[0]['Shot Attempt - Half Court']['Jump Shots'][0][:-1]) * mid_rate, 2)) + '%')
+    mid_makes = float(rice[0]['Jump Shot Range - half court']["Short (<17')"][7]) + \
+                float(rice[0]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][7])
+    mid_total = float(rice[0]['Jump Shot Range - half court']["Short (<17')"][8]) + \
+                float(rice[0]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][8])
+    sheet.update_cell(19, 11, str(round(100 * mid_makes / mid_total, 2)) + '%')
+    mid_pct = (int(rice[0]['Jump Shot Range - half court']["Short (<17')"][4][:-1]) +
+               int(rice[0]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][4][:-1])) // 2
+    sheet.update_cell(18, 12, mid_pct)
+
+    three_rate = float(rice[0]['Shot Attempt - Half Court']['Jump Shots'][0][:-1]) * \
+                 float(rice[0]['Jump Shot Range - half court']["Long (3 point jump shots)"][0][:-1]) / 100
+    sheet.update_cell(20, 11, str(round(three_rate, 2)) + '%')
+    sheet.update_cell(21, 11, rice[0]['Jump Shot Range - half court']["Long (3 point jump shots)"][9])
+    sheet.update_cell(20, 12, rice[0]['Jump Shot Range - half court']["Long (3 point jump shots)"][4][:-1])
+
+    sheet.update_cell(22, 11, rice[2][30])
+    sheet.update_cell(23, 11, str(100 * float(rice[2][5]) / float(rice[2][15])) + '%')
+
+    steal = float(rice[2][8])
+    block = float(rice[2][10])
+    def_poss = float(rice[1]['Overall Defense']["Rice Owls"][1])
+    sheet.update_cell(24, 11, str(100 * steal / def_poss) + '%')
+    sheet.update_cell(25, 11, str(100 * block / def_poss) + '%')
+
+    sheet.update_cell(26, 11, rice[0]['Overall Offense']['Transition'][0])
+    sheet.update_cell(27, 11, rice[0]['Overall Offense']['Transition'][10])
+    sheet.update_cell(26, 12, rice[0]['Overall Offense']['Transition'][4][:-1])
+
+    # -------
+    time.sleep(60)
+
+    sheet.update_cell(12, 13, rice[1]['Overall Defense']["Rice Owls"][10])
+    sheet.update_cell(12, 14, rice[1]['Overall Defense']["Rice Owls"][4][:-1])
+    sheet.update_cell(13, 13, rice[1]['Overall Defense']["Rice Owls"][11])
+    sheet.update_cell(15, 13, rice[1]['Overall Defense']["Rice Owls"][12])
+
+    sheet.update_cell(16, 13, rice[1]['Shot Attempt - Half Court']['Around Basket (not Post-Ups)'][0])
+    sheet.update_cell(17, 13, rice[1]['Shot Attempt - Half Court']['Around Basket (not Post-Ups)'][9])
+    sheet.update_cell(16, 14, rice[1]['Shot Attempt - Half Court']['Around Basket (not Post-Ups)'][4][:-1])
+
+    mid_rate = (float(rice[1]['Jump Shot Range - half court']["Short (<17')"][0][:-1]) +
+                float(rice[1]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][0][:-1])) / 100
+    sheet.update_cell(18, 13, str(
+        round(float(rice[1]['Shot Attempt - Half Court']['Jump Shots'][0][:-1]) * mid_rate, 2)) + '%')
+    mid_makes = float(rice[1]['Jump Shot Range - half court']["Short (<17')"][7]) + \
+                float(rice[1]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][7])
+    mid_total = float(rice[1]['Jump Shot Range - half court']["Short (<17')"][8]) + \
+                float(rice[1]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][8])
+    sheet.update_cell(19, 13, str(round(100 * mid_makes / mid_total, 2)) + '%')
+    mid_pct = (int(rice[1]['Jump Shot Range - half court']["Short (<17')"][4][:-1]) +
+               int(rice[1]['Jump Shot Range - half court']["Medium (17' to <3 point line)"][4][:-1])) // 2
+    sheet.update_cell(18, 14, mid_pct)
+
+    three_rate = float(rice[1]['Shot Attempt - Half Court']['Jump Shots'][0][:-1]) * \
+                 float(rice[1]['Jump Shot Range - half court']["Long (3 point jump shots)"][0][:-1]) / 100
+    sheet.update_cell(20, 13, str(round(three_rate, 2)) + '%')
+    sheet.update_cell(21, 13, rice[1]['Jump Shot Range - half court']["Long (3 point jump shots)"][9])
+    sheet.update_cell(20, 14, rice[1]['Jump Shot Range - half court']["Long (3 point jump shots)"][4][:-1])
+
+    sheet.update_cell(26, 13, rice[1]['Overall Defense']['Transition'][0])
+    sheet.update_cell(27, 13, rice[1]['Overall Defense']['Transition'][10])
+    sheet.update_cell(26, 14, rice[1]['Overall Defense']['Transition'][4][:-1])
+
 
 def main():
-    team = "Purdue Boilermakers"
-    sheet_name = ""
+    team = "Florida International Panthers"
+    sheet_name = '2/19 @ FIU'
 
     data = teamData(team)
     tables = []
@@ -176,7 +343,7 @@ def main():
         rtables.append(process_data(rice[i]))
     rtables.append(process_box(rice[2]))
 
-    google(tables, rtables, sheet_name)
+    google(tables, rtables, sheet_name, team)
 
 
 if __name__ == "__main__":
